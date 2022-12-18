09daeccac4e66e78f9b5aeb0ebb44dc6e584112c
John Uiterwyk <jnuiterwyk@gmail.com>
2022-12-19 01:14:49 +1100

    add voice selector

--- app/speech.js
+++ app/speech.js
@@ -1,5 +1,5 @@
 
-import {addMessageToChatThread} from './chat.js';
+import { addMessageToChatThread } from './chat.js';
 import { sendTextToGPT3 } from './open_ai_api.js';
 
 export function setupSpeech() {
@@ -15,24 +15,22 @@ export function setupSpeech() {
     recognition.interimResults = false;
     recognition.maxAlternatives = 1;
 
+   
 
-    
+
+
     const toggleMicOn = document.querySelector('#toggle-mic-on');
-    toggleMicOn.addEventListener('click', (event) =>{
-        const utterance = new SpeechSynthesisUtterance('listening enabled');
-        window.speechSynthesis.speak(utterance);
-
+    toggleMicOn.addEventListener('click', (event) => {
+        setTimeout(() =>{readTextAloud('listening enabled')}, 500);
         window.recognition.start();
+
 
     })
 
     const toggleMicOff = document.querySelector('#toggle-mic-off');
-    toggleMicOff.addEventListener('click', (event) =>{
-        const utterance = new SpeechSynthesisUtterance('listening disabled');
-        window.speechSynthesis.speak(utterance);
-        
-        window.recognition.abort();
-
+    toggleMicOff.addEventListener('click', (event) => {
+        readTextAloud('listening disabled')
+        setTimeout(() =>{window.recognition.abort()}, 1300);
     })
 
     window.chatHistory = `
@@ -40,7 +38,7 @@ export function setupSpeech() {
     I will do my best to provide you accurate and truthful information. 
     While i will try to be as precise as possible, a simple example of an interaction is as follows:
     user: What is the capital of France?
-    ai: the capital of France is Paris`; 
+    ai: the capital of France is Paris`;
 
     recognition.addEventListener('result', (event) => {
         const input = event.results[event.resultIndex][0].transcript;
@@ -54,12 +52,17 @@ export function setupSpeech() {
     recognition.onend = () => {
         console.log('Speech recognition ended.');
     };
+    
+    populateVoiceList();
+    window.speechSynthesis.addEventListener("voiceschanged", () => {
+        populateVoiceList();
+    });
 }
-export function handleUserInput(input){
+export function handleUserInput(input) {
     addMessageToChatThread('user', input);
 
-    const modifiedPrompt = "user: "+input+ "\nai:";
-    window.chatHistory = window.chatHistory + "\n"+ modifiedPrompt;
+    const modifiedPrompt = "user: " + input + "\nai:";
+    window.chatHistory = window.chatHistory + "\n" + modifiedPrompt;
     sendTextToGPT3(window.chatHistory).then((response) => {
         // Add the input and response to the chat thread
         addMessageToChatThread('ai', response);
@@ -70,9 +73,62 @@ export function handleUserInput(input){
 }
 window.handleUserInput = handleUserInput
 
+function populateVoiceList() {
+    const synth = window.speechSynthesis;
+    const voiceSelect = document.querySelector("#select-voice");
+    let voices = []
+
+    voices = synth.getVoices();
+    voices = voices.sort(function (a, b) {
+        const aname = a.name.toUpperCase();
+        const bname = b.name.toUpperCase();
+
+        if (aname < bname) {
+            return -1;
+        } else if (aname == bname) {
+            return 0;
+        } else {
+            return +1;
+        }
+    });
+    const selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+    voiceSelect.innerHTML = "";
+
+    for (let i = 0; i < voices.length; i++) {
+        const option = document.createElement("option");
+        option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+        if (voices[i].default) {
+            option.textContent += " -- DEFAULT";
+        }
+
+        option.setAttribute("data-lang", voices[i].lang);
+        option.setAttribute("data-name", voices[i].name);
+        if (voices[i].lang.startsWith('en-')) {
+            voiceSelect.appendChild(option);
+        }
+
+    }
+    voiceSelect.selectedIndex = selectedIndex;
+    window.voices = voices;
+}
 // Function to read text aloud
 export function readTextAloud(text) {
     console.log('Reading aloud.');
     const utterance = new SpeechSynthesisUtterance(text);
+
+    // get voice
+    if(window.voices)
+    {
+        const voiceSelect = document.querySelector("#select-voice");
+        const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+        for (let i = 0; i < window.voices.length; i++) {
+            if (voices[i].name === selectedOption) {
+                utterance.voice = window.voices[i];
+                break;
+            }
+        }
+    }
+    
     window.speechSynthesis.speak(utterance);
 }
